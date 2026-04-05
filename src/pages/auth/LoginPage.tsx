4a13e3d88cf147102aed7ebd4f37156233894983
taesungs <70329288+taesungs@users.noreply.github.com>
2026-04-05 22:35:36 +0900

    feat: 로그인 페이지 추가

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,10 @@
+import GithubLoginButton from "../../components/GithubLoginButton";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>로그인</h1>
+      <GithubLoginButton />
+    </div>
+  );
+}
